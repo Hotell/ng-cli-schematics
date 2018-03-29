a518630e63ceafae45e6020b878de8874d85f764
Martin Hochel <hochelmartin@gmail.com>
2018-03-29 15:22:16 +0200

    fix(application): format polyfills to pass jsdoc tslint rule

--- src/application/files/__sourcedir__/polyfills.ts
+++ src/application/files/__sourcedir__/polyfills.ts
@@ -18,7 +18,7 @@
  * BROWSER POLYFILLS
  */
 
-/** IE9, IE10 and IE11 requires all of the following polyfills. **/
+/** IE9, IE10 and IE11 requires all of the following polyfills. */
 // import 'core-js/es6/symbol';
 // import 'core-js/es6/object';
 // import 'core-js/es6/function';
@@ -41,7 +41,7 @@
 // import 'core-js/es6/reflect';
 
 
-/** Evergreen browsers require these. **/
+/** Evergreen browsers require these. */
 // Used for reflect-metadata in JIT. If you use AOT (and only Angular decorators), you can remove.
 import 'core-js/es7/reflect';
 
@@ -49,7 +49,7 @@ import 'core-js/es7/reflect';
 /**
  * Required to support Web Animations `@angular/platform-browser/animations`.
  * Needed for: All but Chrome, Firefox and Opera. http://caniuse.com/#feat=web-animation
- **/
+ */
 // import 'web-animations-js';  // Run `npm install --save web-animations-js`.
 
 /**
