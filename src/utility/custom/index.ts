7db2fb6d2ab8ea06d0bb9a89aec4d2f2646b1a5c
Martin Hochel <hochelmartin@gmail.com>
2018-04-07 16:23:31 +0200

    feat(utils): add custom utils module

---
+++ src/utility/custom/index.ts
@@ -0,0 +1,7 @@
+/**
+ * custom folder module includes all custom utils for this project.
+ *
+ * -> modules in root utility/* are copied from @scehmatics/angular
+ */
+export * from './package';
+export * from './lib-versions';
---
+++ src/utility/custom/lib-versions.ts
@@ -0,0 +1,2 @@
+export const jestVersion = '^5.2.0';
+export const jestPresetAngularVersion = '^5.2.0';
